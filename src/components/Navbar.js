4c9e669e35e321d9f208a6840b5211418e9aafd1
sushiiiii09 <sshreechoudhury@gmail.com>
2023-03-16 22:02:45 +0530

    clean up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,15 +57,6 @@ export default function Navbar() {
               <li tabIndex={0}>
                 <a className="justify-between hover:text-purple-600 hover:bg-[#d5c8fa]">
                   Projects
-                  {/* <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg> */}
                 </a>
               </li>
               <li>
